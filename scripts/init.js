6a129c4667da87a5fcc03ef488ce83b6b89936f1
Quentin Cavi <quentin.cavillier@epfl.ch>
2018-01-27 17:30:45 +0100

    QR code for claim

--- scripts/init.js
+++ scripts/init.js
@@ -1,67 +1,80 @@
-$("#btnVerify").click(function(e) {
+$(".btnVerify").click(function(e) {
+	Scan('verify')
+});
+
+$(".btnClaim").click(function(e) {
+	Scan('claim')
+});
+
+var thisPage = location.hash;
+            if (thisPage != '') {
+                location.href=""
+            }
+	  
+function Scan(codeType){
 	Instascan.Camera.getCameras().then(function(cameras) {
-		if(cameras.length > 0) {
-			scanner.start(cameras[0]);
-		} else {
-			swal({
-				position: 'top-end',
-				type: 'warning',
-				title: 'No camera found'
-			})
-		}
+	if(cameras.length > 0) {
+		scanner.start(cameras[0]);
+	} else {
+		swal({
+			position: 'top-end',
+			type: 'warning',
+			title: 'No camera found'
+		})
+	
+	}
+	
 	})
 	.catch(function(e) {
 		console.error(e);
 	});
 
-	let scanner = new Instascan.Scanner({video: document.getElementById('cameraVerify')});
+	let scanner = new Instascan.Scanner({video: document.getElementById('camera')});
 	scanner.addListener('scan', function(assetId) {
 		$.getJSON("https://network.ambrosus.com/assets/find/BeerCode:" + assetId)
 		.done(function(data) {
 			console.log(data[0]);
 			if(!data[0])alert('No data[0]')
 			if(!data[0].content.data)alert('No data[0].content.data')
-			var ds = data[0].content.data
-			for(var i in ds){
+			var data = data[0].content.data
+			if(codeType=="verify"){
+				Verify(data)
+			}else if(codeType=="claim"){
+				Claim(data)
+			}
+		
+		
+			scanner.stop();
+		});
+	});
+}	
+
+function Claim(data){
+	
+		for(var i in data){
 				if(i=="identifiers")continue
 				$('#BeerInfo').append('<li class="ui-field-contain">'+
             '<label>'+i+'</label>'+
-            '<span>'+ds[i]+'</span>'+        
+            '<span>'+data[i]+'</span>'+        
             '</li>')
 			}
 			
 			
 			$.mobile.changePage("#asset", {transition: "slide"});
-			scanner.stop();
-		});
-	});
-});
+	
+}  
 
-$("#btnClaim").click(function(e) {
-	Instascan.Camera.getCameras().then(function(cameras) {
-		if(cameras.length > 0) {
-			scanner.start(cameras[0]);
-		} else {
-			swal({
-				position: 'top-end',
-				type: 'warning',
-				title: 'No camera found'
-			})
-		}
-	})
-	.catch(function(e) {
-		console.error(e);
-	});
-
-	let scanner = new Instascan.Scanner({video: document.getElementById('cameraClaim')});
-	scanner.addListener('scan', function(assetId) {
-		$.getJSON("https://network.ambrosus.com/assets/find/BeerCode:" + assetId)
-		.done(function(data) {
-			console.log(data[0]);
-			/* $.mobile.changePage("#asset", {transition: "slide"}); */
-			scanner.stop();
-		});
-	});
-});
-	  
-	  
+function Verify(data){
+	
+		for(var i in data){
+				if(i=="identifiers")continue
+				$('#BeerInfo').append('<li class="ui-field-contain">'+
+            '<label>'+i+'</label>'+
+            '<span>'+data[i]+'</span>'+        
+            '</li>')
+			}
+			
+			
+			$.mobile.changePage("#asset", {transition: "slide"});
+	
+}
