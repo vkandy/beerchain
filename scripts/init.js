7798e001332315892373339a727a3f9b56deb7c4
Quentin Cavi <quentin.cavillier@epfl.ch>
2018-01-27 17:02:20 +0100

    Disclose Beer Info

--- scripts/init.js
+++ scripts/init.js
@@ -19,7 +19,18 @@ $("#btnVerify").click(function(e) {
 		$.getJSON("https://network.ambrosus.com/assets/find/BeerCode:" + assetId)
 		.done(function(data) {
 			console.log(data[0]);
-			$('#name').html(name)
+			if(!data[0])alert('No data[0]')
+			if(!data[0].content.data)alert('No data[0].content.data')
+			var ds = data[0].content.data
+			for(var i in ds){
+				if(i=="identifiers")continue
+				$('#BeerInfo').append('<li class="ui-field-contain">'+
+            '<label>'+i+'</label>'+
+            '<span>'+ds[i]+'</span>'+        
+            '</li>')
+			}
+			
+			
 			$.mobile.changePage("#asset", {transition: "slide"});
 			scanner.stop();
 		});
