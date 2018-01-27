cdd9c2287ace900ba4f8adf833aec985cbad460b
vkandy <vkandy@gmail.com>
2018-01-27 16:14:07 +0100

    retrieve asset info from blockchain after scanning the QR code

--- scripts/init.js
+++ scripts/init.js
@@ -1,26 +1,27 @@
-  $("#btnVerify").click(function(e){
-	/*   e.preventDefault(); */
-	  let scanner = new Instascan.Scanner({ video: document.getElementById('preview') });
-      scanner.addListener('scan', function (content) {
-		console.log(content)
-		retrieveAsset(content)
-      });
-	 
-      Instascan.Camera.getCameras().then(function (cameras) {
-		   
-        if (cameras.length > 0) {
-          scanner.start(cameras[0]);
-        } else {
-          swal({
-			  position: 'top-end',
-			  type: 'warning',
-			  title: 'No camera found'
+$("#btnVerify").click(function(e) {
+	let scanner = new Instascan.Scanner({video: document.getElementById('preview')});
+	scanner.addListener('scan', function(assetId) {
+		$.getJSON("https://network.ambrosus.com/assets/find/BeerCode:" + assetId)
+		.done(function(data) {
+			console.log(data[0].content);
+			$.mobile.pagecontainer("change", "#asset", {transition: "slide"});
+		});
+	});
+
+	Instascan.Camera.getCameras().then(function(cameras) {
+		if(cameras.length > 0) {
+			scanner.start(cameras[0]);
+		} else {
+			swal({
+				position: 'top-end',
+				type: 'warning',
+				title: 'No camera found'
 			})
-        }
-      }).catch(function (e) {
-        console.error(e);
-      });
-	  
-  })
+		}
+	}).catch(function(e) {
+		console.error(e);
+	});
+
+});
 	  
 	  
